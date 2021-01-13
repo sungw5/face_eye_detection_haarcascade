0e25e3028ddbf621714aa2c7e88459ee42e0be71
Sung Woo Oh <sungbr95@gmail.com>
2021-01-13 16:14:46 -0500

    face detection for the webcam

---
+++ faceDetector.py
@@ -0,0 +1,51 @@
+import cv2
+import sys
+
+# load the trained data on frontal faces from opencv
+trainedFaceData = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#img = cv2.imread('buspasspic.jpeg')
+videoCapture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = videoCapture.read()
+
+    # convert to grayscale (!IMPORTANT)
+    grayscaledImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # gives coordinates (x,y,w,h) in 2d array
+    faceCoordinates = trainedFaceData.detectMultiScale(grayscaledImg)
+
+    
+    for i in range(len(faceCoordinates)):
+        (x, y, w, h) = faceCoordinates[i]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # imgfile, coordinates1, coordinates2, color, thickness
+        print(x,y,w,h)
+
+    cv2.imshow('Video', frame) #display
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+videoCapture.release()
+cv2.destroyAllWindows()
+print("************  Exit Camera  ************ ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Tyler's faceDetector") 
